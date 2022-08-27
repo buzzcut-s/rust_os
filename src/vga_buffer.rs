buzzcut-s/rust_os

commit d2edde7c01c38da1e1b73e03da7833b7cbd9cf48

vga_buffer: Implement Writer type
The writer will always write to the last line and shift lines up when
a line is full. The new_line() function will be implemented soon.

Rust strings are UTF-8 by default, so they might contain bytes that are
not supported by the VGA text buffer. We use a match to differentiate
printable ASCII bytes (a newline or anything in between a space character
and a ~ character) and unprintable bytes. For unprintable bytes, we print
a ■ character, which has the hex code 0xfe on the VGA hardware.

Signed-off-by: Piyush Kumar <piyushkumg@gmail.com>

src/vga_buffer.rs
@@ -44,3 +44,43 @@ const BUFFER_WIDTH: usize = 80;
 struct Buffer {
     chars: [[ScreenChar; BUFFER_WIDTH]; BUFFER_HEIGHT],
 }
+
+pub struct Writer {
+    column_position: usize,
+    color_code: ColorCode,
+    buffer: &'static mut Buffer,
+}
+
+impl Writer {
+    pub fn write_byte(&mut self, byte: u8) {
+        match byte {
+            b'\n' => self.new_line(),
+            byte => {
+                if self.column_position >= BUFFER_WIDTH {
+                    self.new_line();
+                }
+
+                let row = BUFFER_HEIGHT - 1;
+                let col = self.column_position;
+
+                let color_code = self.color_code;
+                self.buffer.chars[row][col] = ScreenChar {
+                    ascii_character: byte,
+                    color_code,
+                };
+                self.column_position += 1;
+            }
+        }
+    }
+
+    pub fn write_string(&mut self, s: &str) {
+        for byte in s.bytes() {
+            match byte {
+                0x20..=0x7e => self.write_byte(byte),
+                _ => self.write_byte(0xfe),
+            }
+        }
+    }
+
+    fn new_line(&mut self) {}
+}

commit 6cd641b99f9ce61b372749ed8715ccdc334c8967

vga_buffer: Fix matching newline in write_string
Accidentally missed this.

Fixes: d2edde7c01c38da1e1b73e03da7833b7cbd9cf48 ("vga_buffer: Implement Writer type")

Signed-off-by: Piyush Kumar <piyushkumg@gmail.com>

src/vga_buffer.rs
@@ -81,7 +81,7 @@ impl Writer {
     pub fn write_string(&mut self, s: &str) {
         for byte in s.bytes() {
             match byte {
-                0x20..=0x7e => self.write_byte(byte),
+                0x20..=0x7e | b'\n' => self.write_byte(byte),
                 _ => self.write_byte(0xfe),
             }
         }