464a16efe4f1933d2349e4eeb42569ff9ff28736
Piyush Kumar <piyushkumg@gmail.com>
2022-08-27 18:14:58 +0530

    vga_buffer: Implement new_line for Writer
    Pretty straightforward, we move every character one line up and the top
    line gets deleted and clears the row by setting every character as space.
    
    Signed-off-by: Piyush Kumar <piyushkumg@gmail.com>

--- src/vga_buffer.rs
+++ src/vga_buffer.rs
@@ -85,7 +85,26 @@ impl Writer {
         }
     }
 
-    fn new_line(&mut self) {}
+    fn new_line(&mut self) {
+        for row in 1..BUFFER_HEIGHT {
+            for col in 0..BUFFER_WIDTH {
+                let c = self.buffer.chars[row][col].read();
+                self.buffer.chars[row - 1][col].write(c)
+            }
+        }
+        self.clear_row(BUFFER_HEIGHT - 1);
+        self.column_position = 0;
+    }
+
+    fn clear_row(&mut self, row: usize) {
+        let blank = ScreenChar {
+            ascii_character: b' ',
+            color_code: self.color_code,
+        };
+        for col in 0..BUFFER_WIDTH {
+            self.buffer.chars[row][col].write(blank);
+        }
+    }
 }
 
 impl fmt::Write for Writer {
@@ -106,5 +125,6 @@ pub fn print_something() {
 
     writer.write_byte(b'W');
     writer.write_string("itness");
+    writer.write_byte(b'\n');
     write!(writer, "The numbers are {} and {}", 42, 1.0 / 3.0).unwrap();
 }
