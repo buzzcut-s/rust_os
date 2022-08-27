2c461472f6cb25988791e741e2fa4a6f3042ead8
Piyush Kumar <piyushkumg@gmail.com>
2022-08-27 18:03:49 +0530

    main: Witness, but with vga_buffer
    Signed-off-by: Piyush Kumar <piyushkumg@gmail.com>

--- src/vga_buffer.rs
+++ src/vga_buffer.rs
@@ -84,3 +84,14 @@ impl Writer {
 
     fn new_line(&mut self) {}
 }
+
+pub fn print_something() {
+    let mut writer = Writer {
+        column_position: 0,
+        color_code: ColorCode::new(Color::Yellow, Color::Black),
+        buffer: unsafe { &mut *(0xb8000 as *mut Buffer) },
+    };
+
+    writer.write_byte(b'W');
+    writer.write_string("itness");
+}
